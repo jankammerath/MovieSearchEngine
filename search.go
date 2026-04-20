6a7771c4b90ef0bec3986144181301a263320b6b
Jan Kammerath <jankammerath@gmail.com>
2026-04-20 09:55:27 +0200

    constructor for search engine

--- search.go
+++ search.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type MovieItem struct {
 	movieId string
 	title   string
@@ -11,4 +13,42 @@ type SearchEngine struct {
 	movies   []MovieItem
 	yearMap  map[int16][]*MovieItem
 	genreMap map[string][]*MovieItem
+}
+
+func NewSearchEngine(movies []TitleBasic) *SearchEngine {
+	engine := &SearchEngine{
+		movies:   make([]MovieItem, 0, len(movies)),
+		yearMap:  make(map[int16][]*MovieItem),
+		genreMap: make(map[string][]*MovieItem),
+	}
+
+	for _, title := range movies {
+		if title.TitleType != "movie" {
+			continue
+		}
+
+		var year int16
+		if title.StartYear != "\\N" {
+			fmt.Sscanf(title.StartYear, "%d", &year)
+		}
+
+		movie := MovieItem{
+			movieId: title.TConst,
+			title:   title.PrimaryTitle,
+			year:    year,
+			genres:  title.Genres,
+		}
+
+		engine.movies = append(engine.movies, movie)
+
+		if year != 0 {
+			engine.yearMap[year] = append(engine.yearMap[year], &engine.movies[len(engine.movies)-1])
+		}
+
+		for _, genre := range title.Genres {
+			engine.genreMap[genre] = append(engine.genreMap[genre], &engine.movies[len(engine.movies)-1])
+		}
+	}
+
+	return engine
 }
